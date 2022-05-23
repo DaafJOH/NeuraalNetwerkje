ecb8b76b547c4c7ec58fbde64325f7890929429d
ThomasBenten <superthomas134@gmail.com>
2022-05-23 13:05:54 +0200

    changes

--- Profielwerkstuk.py
+++ Profielwerkstuk.py
@@ -1,6 +1,6 @@
 import numpy as np
 import math
-#Preparation
+# Preparation
 i = np.matrix([
     [0, 0, 0], 
     [1, 0, 0], 
@@ -24,11 +24,12 @@ w2 = np.matrix([[0.5],
     [0.3],
     [0.2]])
 lr = 0.007
+
 # Functions
 def sigmoid(x):
     return 1/(1+np.exp(x))
 def dsigmoid(x):
-    return np.multiply(sigmoid(x),(1-sigmoid(x)))
+    return np.multiply(sigmoid(x), (1-sigmoid(x)))
 # Loop
 for k in range(1000):
     # Forward
@@ -37,9 +38,10 @@ for k in range(1000):
 
     x2 = y1 * w2
     y2 = sigmoid(x2)
-    
-    #Backward
-    deltaN = np.multiply((y2-t),np.multiply(y2,(1-y2)))
+
+    a = np.multiply(y2,(1-y2))
+    # Backward
+    deltaN = np.multiply((y2-t), a)
     deltan = np.multiply(deltaN, dsigmoid(x2))*w2.transpose()
     w2 = w2 - lr * (deltaN.transpose()*y1).transpose()
     w1 = w1 - lr * (i.transpose()*np.multiply(deltan, dsigmoid(x1)))
---
+++ test.py
@@ -0,0 +1,12 @@
+import numpy as np
+a = np.matrix([
+    [1, 2, 3],
+    [2, 3, 4]
+])
+b = np.matrix([
+    [7, 6],
+    [6, 5],
+    [5, 4],
+    [4, 3]
+])
+print(b*a)
